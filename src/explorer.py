2c355eb3fa7ba0e4bb920ac712d3c1da763269b8
August Soderberg <asoderberg@brandeis.edu>
2021-04-03 19:55:48 +0000

    Changed referenced package names

--- src/explorer.py
+++ src/explorer.py
@@ -6,7 +6,7 @@
 
 import rospy
 from geometry_msgs.msg import Twist
-from line_follower_pa_lisandro.msg import img_data
+from racetrackpa.msg import img_data
 from constants import *
 
 def img_data_cb(msg):
